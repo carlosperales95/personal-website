cc23fe84717217784b6c6e71ca9df98967b427c1
carlosperales95 <carlos.perales95@opendeusto.es>
2021-04-15 17:52:36 +0200

    Trying out something with the emails

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -26,17 +26,17 @@ const Contact = () => {
                     <Icon to="/personal-website">CaPe</Icon>
                     <FormContent>
                         <Form action="mailto:carlos.perales95@opendeusto.es"
-                        method="POST"
-                        enctype="multipart/form-data"
+                        method="get"
+                        enctype="text/plain"
                         name="EmailForm">
                             <FormHeader>Send me an email !</FormHeader>
                             <FormLabel htmlFor='for'>email</FormLabel>
-                            <FormInput type='email' required/>
+                            <FormInput type='email' name="email" required/>
                             <br></br>
                             <FormLabel htmlFor='for'>Message</FormLabel>
-                            <FormArea type='content' required/>
+                            <FormArea type='text' name="content" required/>
                             <ButtonArea>
-                                <FormButton type='submit'>Send!</FormButton>
+                                <FormButton type='submit' name="submit">Send!</FormButton>
                                 {/* <FakeFormButton id="fakebtn" type="button" value="Send!"  onclick="$('#msg').html('you got me');"/>                           */}
                             </ButtonArea>
                         </Form>
