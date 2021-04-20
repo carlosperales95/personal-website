936f706cab9b08b2a7695a9e09eb728e990ecbc1
carlosperales95 <carlos.perales95@opendeusto.es>
2021-04-20 19:19:23 +0200

    New content for Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -20,34 +20,34 @@ const Footer = () => {
             <FooterWrapper>
                 <FooterLinksContainer>
                     <FooterLinksWrapper>
+
                         <FooterLinkItems>
-                            <FooterLinkTitle>Link Section 1</FooterLinkTitle>
-                            <FooterLink to="/personal-website/contact">Link1</FooterLink>
-                            <FooterLink to="/personal-website/contact">LInk2</FooterLink>
-                            <FooterLink to="/personal-website/contact">LInk3</FooterLink>
-                            <FooterLink to="/personal-website/contact">Link4</FooterLink>
-                            <FooterLink to="/personal-website/contact">Link5</FooterLink>
+                            <FooterLinkTitle>Website Index</FooterLinkTitle>
+                            <FooterLink to="/personal-website">Home</FooterLink>
+                            <FooterLink to="/personal-website/contact">Contact</FooterLink>
                         </FooterLinkItems>
                         <FooterLinkItems>
-                            <FooterLinkTitle>Link Section 2</FooterLinkTitle>
-                            <FooterLink to="/personal-website/contact">Link1</FooterLink>
-                            <FooterLink to="/personal-website/contact">LInk2</FooterLink>
-                            <FooterLink to="/personal-website/contact">LInk3</FooterLink>
-                            <FooterLink to="/personal-website/contact">Link4</FooterLink>
-                            <FooterLink to="/personal-website/contact">Link5</FooterLink>
+                            <FooterLinkTitle>Media banks</FooterLinkTitle>
+                            <FooterLink href="https://www.pexels.com/">Pexels</FooterLink>
+                            <FooterLink href="https://www.manypixels.co/gallery">Manypixels</FooterLink>
+                            <FooterLink href="https://absurd.design/">Absurd Design</FooterLink>
+                            <FooterLink href="https://www.drawkit.io/#browse-now-button">Drawkit</FooterLink>
+                            <FooterLink href="https://iradesign.io/">IRA Design</FooterLink>
+                            <FooterLink href="https://undraw.co/illustrations">Undraw</FooterLink>
+                            <FooterLink href="https://icons8.com/illustrations">Ouch</FooterLink>
                         </FooterLinkItems>
                     </FooterLinksWrapper>
                     <FooterLinksWrapper>
-                        <FooterLinkItems>
-                            <FooterLinkTitle>Link Section 3</FooterLinkTitle>
-                            <FooterLink to="/personal-website/contact">Link1</FooterLink>
-                            <FooterLink to="/personal-website/contact">LInk2</FooterLink>
-                            <FooterLink to="/personal-website/contact">LInk3</FooterLink>
-                            <FooterLink to="/personal-website/contact">Link4</FooterLink>
-                            <FooterLink to="/personal-website/contact">Link5</FooterLink>
+                    <FooterLinkItems>
+                            <FooterLinkTitle>Universities</FooterLinkTitle>
+                            <FooterLink href="https://www.deusto.es/cs/Satellite/deusto/en/university-deusto?cambioidioma=si">University of Deusto</FooterLink>
+                            <FooterLink href="https://www.uu.se/en/">University of Uppsala</FooterLink>
+                            <FooterLink href="https://www.vu.nl/en/">VU Amsterdam</FooterLink>
+                            <FooterLink href="https://www.uva.nl/en">University of Amsterdam</FooterLink>
+                            <FooterLink href="https://www.unibo.it/en/homepage">University of Bologna</FooterLink>
                         </FooterLinkItems>
                         <FooterLinkItems>
-                            <FooterLinkTitle>Link Section 4</FooterLinkTitle>
+                            <FooterLinkTitle>Where to find me</FooterLinkTitle>
                             <FooterExternalLink href="https://github.com/carlosperales95">Ghub</FooterExternalLink>
                             <FooterExternalLink href="https://www.mixcloud.com/perlballota/">Mixcloud</FooterExternalLink>
                             <FooterExternalLink href="https://www.instagram.com/cp.raw/">Insta</FooterExternalLink>
